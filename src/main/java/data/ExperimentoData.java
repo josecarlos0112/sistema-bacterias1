90b9590389b3c757a38a09b7b106327cd92a9c2e
josecarlos0112 <jzorrgar@myuax.com>
2024-05-21 22:09:25 +0200

    actualizacion del paquete data, modificacion para cargar y guardar informacion

--- src/main/java/data/ExperimentoData.java
+++ src/main/java/data/ExperimentoData.java
@@ -1,34 +1,56 @@
 package data;
 
+import logica.PoblacionBacteriasManager;
 import logica.ExperimentoManager;
+
 import java.io.*;
+import java.time.LocalDate;
+import java.util.StringJoiner;
 
 public class ExperimentoData {
     public void guardarDatos(ExperimentoManager experimento, String filename) {
-        try {
-            FileOutputStream fileOut = new FileOutputStream(filename);
-            ObjectOutputStream out = new ObjectOutputStream(fileOut);
-            out.writeObject(experimento);
-            out.close();
-            fileOut.close();
-        } catch (IOException i) {
-            i.printStackTrace();
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            for (PoblacionBacteriasManager poblacion : experimento.getPopulations()) {
+                StringJoiner joiner = new StringJoiner("|");
+                joiner.add(poblacion.getName())
+                        .add(String.valueOf(poblacion.getStartDate()))
+                        .add(String.valueOf(poblacion.getEndDate()))
+                        .add(String.valueOf(poblacion.getInitialBacteriaCount()))
+                        .add(String.valueOf(poblacion.getTemperature()))
+                        .add(String.valueOf(poblacion.getLightCondition()))
+                        .add(String.valueOf(poblacion.getInitialFood()))
+                        .add(String.valueOf(poblacion.getIncrementDay()))
+                        .add(String.valueOf(poblacion.getIncrementFood()))
+                        .add(String.valueOf(poblacion.getFinalFood()));
+                writer.write(joiner.toString());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
     public ExperimentoManager cargarDatos(String filename) {
-        ExperimentoManager experimento = null;
-        try {
-            FileInputStream fileIn = new FileInputStream(filename);
-            ObjectInputStream in = new ObjectInputStream(fileIn);
-            experimento = (ExperimentoManager) in.readObject();
-            in.close();
-            fileIn.close();
-        } catch (IOException i) {
-            i.printStackTrace();
-        } catch (ClassNotFoundException c) {
-            System.out.println("Experimento no encontrado");
-            c.printStackTrace();
+        ExperimentoManager experimento = new ExperimentoManager();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split("\\|");
+                PoblacionBacteriasManager poblacion = new PoblacionBacteriasManager();
+                poblacion.setName(parts[0]);
+                poblacion.setStartDate(LocalDate.ofEpochDay(Integer.parseInt(parts[1])));
+                poblacion.setEndDate(LocalDate.ofEpochDay(Integer.parseInt(parts[2])));
+                poblacion.setInitialBacteriaCount(Integer.parseInt(parts[3]));
+                poblacion.setTemperature(Double.parseDouble(parts[4]));
+                poblacion.setLightCondition(String.valueOf(Boolean.parseBoolean(parts[5])));
+                poblacion.setInitialFood(Integer.parseInt(parts[6]));
+                poblacion.setIncrementDay(Integer.parseInt(parts[7]));
+                poblacion.setIncrementFood(Integer.parseInt(parts[8]));
+                poblacion.setFinalFood(Integer.parseInt(parts[9]));
+                experimento.addPopulation(poblacion);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return experimento;
     }
--- src/main/java/data/PoblacionBacteriasData.java
+++ src/main/java/data/PoblacionBacteriasData.java
@@ -1,34 +1,52 @@
 package data;
 
 import logica.PoblacionBacteriasManager;
+
 import java.io.*;
+import java.time.LocalDate;
+import java.util.StringJoiner;
 
 public class PoblacionBacteriasData {
     public void guardarDatos(PoblacionBacteriasManager poblacion, String filename) {
-        try {
-            FileOutputStream fileOut = new FileOutputStream(filename);
-            ObjectOutputStream out = new ObjectOutputStream(fileOut);
-            out.writeObject(poblacion);
-            out.close();
-            fileOut.close();
-        } catch (IOException i) {
-            i.printStackTrace();
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            StringJoiner joiner = new StringJoiner("|");
+            joiner.add(poblacion.getName())
+                    .add(String.valueOf(poblacion.getStartDate()))
+                    .add(String.valueOf(poblacion.getEndDate()))
+                    .add(String.valueOf(poblacion.getInitialBacteriaCount()))
+                    .add(String.valueOf(poblacion.getTemperature()))
+                    .add(String.valueOf(poblacion.getLightCondition()))
+                    .add(String.valueOf(poblacion.getInitialFood()))
+                    .add(String.valueOf(poblacion.getIncrementDay()))
+                    .add(String.valueOf(poblacion.getIncrementFood()))
+                    .add(String.valueOf(poblacion.getFinalFood()));
+            writer.write(joiner.toString());
+            writer.newLine();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
     public PoblacionBacteriasManager cargarDatos(String filename) {
         PoblacionBacteriasManager poblacion = null;
-        try {
-            FileInputStream fileIn = new FileInputStream(filename);
-            ObjectInputStream in = new ObjectInputStream(fileIn);
-            poblacion = (PoblacionBacteriasManager) in.readObject();
-            in.close();
-            fileIn.close();
-        } catch (IOException i) {
-            i.printStackTrace();
-        } catch (ClassNotFoundException c) {
-            System.out.println("Poblacion de Bacterias no encontrada");
-            c.printStackTrace();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split("\\|");
+                poblacion = new PoblacionBacteriasManager();
+                poblacion.setName(parts[0]);
+                poblacion.setStartDate(LocalDate.ofEpochDay(Integer.parseInt(parts[1])));
+                poblacion.setEndDate(LocalDate.ofEpochDay(Integer.parseInt(parts[2])));
+                poblacion.setInitialBacteriaCount(Integer.parseInt(parts[3]));
+                poblacion.setTemperature(Double.parseDouble(parts[4]));
+                poblacion.setLightCondition(String.valueOf(Boolean.parseBoolean(parts[5])));
+                poblacion.setInitialFood(Integer.parseInt(parts[6]));
+                poblacion.setIncrementDay(Integer.parseInt(parts[7]));
+                poblacion.setIncrementFood(Integer.parseInt(parts[8]));
+                poblacion.setFinalFood(Integer.parseInt(parts[9]));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return poblacion;
     }
